9f7029b0575babe1c16cfc150c976fbf6e03e842
Lucio Franco <lucio.franco@du.edu>
2015-07-18 18:08:03 -0600

    updated the model index type system

--- test/model.test.js
+++ test/model.test.js
@@ -31,7 +31,8 @@ describe('Model:', function () {
         password: String,
         age: Number
       });
-      var User = Rebound.model('User', {},UserSchema);
+      var User = Rebound.modelIndex('test', 'User', UserSchema);
+      console.log(Rebound);
       User.should.be.ok;
       User.should.have.properties(['connection', 'schema']);
       User.schema.should.be.instanceof(Schema);
